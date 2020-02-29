83429e603f1bb8e751f3f1cdf328ed484c55aca9
Tim Petrvalsky <tpet55@yahoo.com>
2020-02-29 12:40:43 -0800

    Final commit, finished

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-console.log('Hello');
-
 let mainArea = document.querySelector('.mainContent');
 let rowsAndColumns = prompt('How many rows and columns do you want for your board?');
 let square = document.querySelector('.square');
 
+// Create Board Grid
 function createDivs(rowsAndColumns) {
 	mainArea.style.gridTemplateColumns = `repeat(${rowsAndColumns}, 40px)`;
 	mainArea.style.gridTemplateRows = `repeat(${rowsAndColumns}, 40px)`;
@@ -12,33 +11,33 @@ function createDivs(rowsAndColumns) {
 		const square = document.createElement('div');
 		square.classList.add('square');
 		mainArea.appendChild(square);
+
+		// create Random color function to color board
+		function random_rgba() {
+			var o = Math.round;
+			var r = Math.random;
+			var s = 255;
+			return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + 1 + ')';
+		}
+
+		let colorBoard = square.addEventListener('mouseover', function(event) {
+			event.target.style.backgroundColor = random_rgba();
+		});
 	}
 }
-
 let selector = document.querySelector('.mainContent');
 let clear = document.querySelector('#clear');
 let change = document.querySelector('#change');
 
-let colorBoard = selector.addEventListener('mouseover', function(event) {
-	event.target.style.backgroundColor = 'blue';
-	console.log(event);
-});
-
-let itemsList = document.querySelectorAll('.square');
-itemsList.forEach((item) => {
-	item.addEventListener('mouseover', () => {
-		colorBoard(event);
-	});
-});
-
+// Clear the board
 let clearBoard = clear.addEventListener('click', function() {
 	let cells = document.querySelectorAll('.square');
-	console.log(clear);
 	cells.forEach((square) => {
 		square.style.backgroundColor = 'white';
 	});
 });
 
+//Get user input to change the board size
 let changeBoardSize = change.addEventListener('click', function() {
 	window.location.reload();
 });
